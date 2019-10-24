0a7fade02d70873c42e454f96d4b024efe7fd81a
Beuc <beuc@beuc.net>
2019-10-24 11:12:32 +0000

    emscripten.pyx: better support for set_main_loop with memory management; clarifications

--- emscripten.pyx
+++ emscripten.pyx
@@ -22,6 +22,7 @@ cdef extern from "emscripten.h":
 
     void emscripten_set_main_loop(em_callback_func func, int fps, int simulate_infinite_loop)
     void emscripten_set_main_loop_arg(em_arg_callback_func func, void *arg, int fps, int simulate_infinite_loop)
+    void emscripten_cancel_main_loop()
     void emscripten_exit_with_live_runtime()
     void emscripten_sleep(unsigned int ms)
     void emscripten_sleep_with_yield(unsigned int ms)
@@ -116,37 +117,94 @@ from libc.string cimport strncpy
 #    int puts(const char *s);
 
 
+# C callback - no memory management
+# Take a single Python object and calls it
 cdef void callpyfunc(void *py_function):
     # not necessary as we're using a no-threading Python
     #PyEval_InitThreads()
     # Call Python function from C using (<object>)()
-    (<object>py_function)()
+    f = <object>py_function
+    f()
 
-cdef struct callpyfunc_s:
+
+# C callbacks - memory management
+cdef struct callpyfunc_arg_s:
     PyObject* py_function
-    PyObject* arg
-cdef void callpyfunc_arg(void* p):
-    s = <callpyfunc_s*>p
-    py_function = <object>(s.py_function)
-    arg = <object>(s.arg)
-    (py_function)(arg)
-    Py_XDECREF(s.py_function)
-    Py_XDECREF(s.arg)
-    free(s)
+    PyObject* py_arg
 
+# Take a Python object and calls it ONCE on passed argument
+# C callback for e.g. emscripten_async_call
+cdef void callpyfunc_arg_once(void* p):
+    s = <callpyfunc_arg_s*>p
+    callpyfunc_arg_nofree(s)
+    freepyfunc(s)
+
+# Take a Python object and calls it on passed argument
+# C callback for e.g. emscripten_set_main_loop_arg
+cdef void callpyfunc_arg_nofree(void* p):
+    s = <callpyfunc_arg_s*>p
+    py_function = <object>(s.py_function)
+    if s.py_arg != NULL:
+        py_arg = <object>(s.py_arg)
+        py_function(py_arg)
+    else:
+        py_function()
+
+cdef callpyfunc_arg_s* createpyfunc(PyObject* py_function, PyObject* py_arg):
+    cdef callpyfunc_arg_s* s = <callpyfunc_arg_s*> PyMem_Malloc(sizeof(callpyfunc_arg_s))
+    s.py_function = py_function
+    s.py_arg = py_arg
+    Py_XINCREF(s.py_function)
+    Py_XINCREF(s.py_arg)    
+    return s
+
+cdef void freepyfunc(callpyfunc_arg_s *p):
+    if p != NULL:
+        Py_XDECREF(p.py_function)
+        if p.py_arg != NULL:
+          Py_XDECREF(p.py_arg)
+        PyMem_Free(p)
+
+
+cdef callpyfunc_arg_s* main_loop
+
+def set_main_loop_arg(py_function, py_arg, fps, simulate_infinite_loop):
+    set_main_loop_arg_c(<PyObject*>py_function, <PyObject*>py_arg,
+                        fps, simulate_infinite_loop)
 
 def set_main_loop(py_function, fps, simulate_infinite_loop):
-    #print "def: set_main_loop", py_function, fps, simulate_infinite_loop
-    emscripten_set_main_loop_arg(callpyfunc, <PyObject*>py_function, fps, simulate_infinite_loop)
+    set_main_loop_arg_c(<PyObject*>py_function, NULL,
+                        fps, simulate_infinite_loop)
 
-def async_call(func, arg, millis):
-    #print "def: async_call", func, arg, millis
-    cdef callpyfunc_s* s = <callpyfunc_s*> malloc(sizeof(callpyfunc_s))
-    s.py_function = <PyObject*>func
-    s.arg = <PyObject*>arg
-    Py_XINCREF(s.py_function)
-    Py_XINCREF(s.arg)
-    emscripten_async_call(callpyfunc_arg, <void*>s, millis)
+# handle py_arg == NULL != None
+cdef set_main_loop_arg_c(PyObject* py_function, PyObject* py_arg,
+                         fps, simulate_infinite_loop):
+    global main_loop
+    if main_loop == NULL:
+        main_loop = createpyfunc(py_function, py_arg)
+    else:
+        pass  # invalid, let emscripten_set_main_loop_arg() abort
+    emscripten_set_main_loop_arg(callpyfunc_arg_nofree, <void*>main_loop,
+                                 fps, simulate_infinite_loop)
+
+def cancel_main_loop():
+    global main_loop
+    emscripten_cancel_main_loop()
+    freepyfunc(main_loop)
+    main_loop = NULL
+
+# import emscripten,sys
+# emscripten.set_main_loop(lambda: sys.stdout.write("main_loop\n"), 2, 0)
+# emscripten.cancel_main_loop()
+# emscripten.set_main_loop(lambda: sys.stdout.write("main_loop\n"), 2, 1)
+# emscripten.set_main_loop_arg(lambda a: sys.stdout.write(a), "main_loop_arg\n", 2, 0)
+
+def async_call(py_function, py_arg, millis):
+    cdef callpyfunc_arg_s* s = createpyfunc(<PyObject*>py_function, <PyObject*>py_arg)
+    emscripten_async_call(callpyfunc_arg_once, <void*>s, millis)
+
+# emscripten.async_call(lambda a: sys.stdout.write(a), "async_call_arg\n", 2000)
+
 
 def exit_with_live_runtime():
     emscripten_exit_with_live_runtime();
@@ -165,6 +223,7 @@ def run_script_int(script):
 
 def run_script_string(script):
     return emscripten_run_script_string(script.encode('UTF-8'));
+
 
 # async_wget
 # Requires a C function without parameter, while we need to set
@@ -212,6 +271,9 @@ def async_wget_data(url, arg, onload, onerror=None):
 # emscripten.async_wget_data('https://bank.confidential/', None, None)
 
 
+
+# Fetch API
+# https://emscripten.org/docs/api_reference/fetch.html
 
 # http://docs.cython.org/en/latest/src/userguide/extension_types.html
 cdef class Fetch:
@@ -445,6 +507,9 @@ cdef void callpyfunc_fetch_onreadystatechange(emscripten_fetch_t *fetch):
 # r=emscripten.Fetch('/hello', attributes=emscripten.FETCH_LOAD_TO_MEMORY)
 # open('test.txt','wb').write(r); open('test.txt','rb').read()
 # open('test.txt','wb').write(r.data); open('test.txt','rb').read()
+
+
+# Non-API utility
 
 def syncfs():
     emscripten_run_script(r"""
