bff0fcf4f1bbc9df1bbd8c0f97b70a9aef5923a2
Beuc <beuc@beuc.net>
2019-11-04 21:47:35 +0000

    package-pythonhome.sh: optimize _sysconfigdata

--- 2.7.10/package-pythonhome.sh
+++ 2.7.10/package-pythonhome.sh
@@ -14,7 +14,7 @@ FILE_PACKAGER="python $(dirname $(which emcc))/tools/file_packager.py"
 PREFIX=${PREFIX:-$(dirname $(readlink -f $0))/destdir}
 PACKAGEDIR=${PACKAGEDIR:-$(dirname $(readlink -f $0))/package}
 OUTDIR=${OUTDIR:-.}
-HOSTPYTHON=$(dirname $(readlink -f $0))/build/hostpython/bin/python
+CROSSPYTHON=$(dirname $(readlink -f $0))/crosspython-static/bin/python
 
 # Python home
 
@@ -27,14 +27,18 @@ mkdir -p $PACKAGEDIR
 
 # Hard-coded modules: for 'print("hello, world.")'
 # $@: additional, app-specific modules
-for i in site.py os.py posixpath.py stat.py genericpath.py warnings.py linecache.py types.py UserDict.py _abcoll.py abc.py _weakrefset.py copy_reg.py traceback.py sysconfig.py re.py sre_compile.py sre_parse.py sre_constants.py _sysconfigdata.py encodings/__init__.py codecs.py encodings/aliases.py encodings/utf_8.py __future__.py ast.py copy.py weakref.py platform.py string.py io.py tempfile.py random.py hashlib.py struct.py dummy_thread.py collections.py keyword.py heapq.py argparse.py textwrap.py gettext.py locale.py functools.py importlib/__init__.py glob.py fnmatch.py pickle.py colorsys.py contextlib.py zipfile.py shutil.py json/__init__.py json/decoder.py json/scanner.py encodings/hex_codec.py json/encoder.py difflib.py inspect.py dis.py opcode.py tokenize.py token.py xml/__init__.py xml/etree/__init__.py xml/etree/ElementTree.py xml/etree/ElementPath.py encodings/zlib_codec.py tarfile.py urlparse.py StringIO.py encodings/latin_1.py encodings/ascii.py \
+for i in site.py os.py posixpath.py stat.py genericpath.py warnings.py linecache.py types.py UserDict.py _abcoll.py abc.py _weakrefset.py copy_reg.py traceback.py sysconfig.py re.py sre_compile.py sre_parse.py sre_constants.py encodings/__init__.py codecs.py encodings/aliases.py encodings/utf_8.py __future__.py ast.py copy.py weakref.py platform.py string.py io.py tempfile.py random.py hashlib.py struct.py dummy_thread.py collections.py keyword.py heapq.py argparse.py textwrap.py gettext.py locale.py functools.py importlib/__init__.py glob.py fnmatch.py pickle.py colorsys.py contextlib.py zipfile.py shutil.py json/__init__.py json/decoder.py json/scanner.py encodings/hex_codec.py json/encoder.py difflib.py inspect.py dis.py opcode.py tokenize.py token.py xml/__init__.py xml/etree/__init__.py xml/etree/ElementTree.py xml/etree/ElementPath.py encodings/zlib_codec.py tarfile.py urlparse.py StringIO.py encodings/latin_1.py encodings/ascii.py \
     "$@"; do
     if [ $PREFIX/lib/python2.7/$i -nt $PREFIX/lib/python2.7/${i%.py}.pyo ]; then
-	(cd $PREFIX && $HOSTPYTHON -OO -m py_compile lib/python2.7/$i)
+	(cd $PREFIX && $CROSSPYTHON -OO -m py_compile lib/python2.7/$i)
     fi
     mkdir -p $PACKAGEDIR/lib/python2.7/$(dirname $i)
     cp -au $PREFIX/lib/python2.7/${i%.py}.pyo $PACKAGEDIR/lib/python2.7/${i%.py}.pyo
 done
+# Large and leaks build paths, clean it:
+echo 'build_time_vars = {}' > $PACKAGEDIR/lib/python2.7/_sysconfigdata.py
+(cd $PACKAGEDIR && $CROSSPYTHON -OO -m py_compile lib/python2.7/_sysconfigdata.py)
+rm -f $PACKAGEDIR/lib/python2.7/_sysconfigdata.py
 
 # --no-heap-copy: suited for ALLOW_MEMORY_GROWTH=1
 PACKAGEDIR_FULLPATH=$(readlink -f $PACKAGEDIR)
